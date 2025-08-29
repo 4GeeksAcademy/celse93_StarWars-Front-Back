df4470a11eabe0865ce405ef17720e2e6d77f0b3
PASM <157536097+celse93@users.noreply.github.com>
2025-08-29 18:17:22 +0000

    second commit

--- src/routes/characters.py
+++ src/routes/characters.py
@@ -9,14 +9,47 @@ def register_characters_routes(app):
     def characters_collection():
         response_body = {}
         if request.method == "GET":
-            url = 'https://swapi.info/api/people'
-            response = requests.get(url)
+            # Check if characters exists in DB
+            characters = db.session.scalars(db.select(Characters)).all()
 
-            if response.status_code == 200:
-                data = response.json()
-                response_body = data
+            if not characters:
+                url = 'https://swapi.info/api/people'
+                response = requests.get(url)
 
-            return response_body, 200
+                if response.status_code == 200:
+                    data = response.json()
+                    for charac in data:
+                        # get IDs from data properties' url value
+                        character_id = int(charac["url"].split("/")[-1])
+                        planet_id = int(charac["homeworld"].split("/")[-1])
+                        if len(charac["species"]) != 0:
+                            specie_id = int(
+                                charac["species"][0].split("/")[-1])
+                        else:
+                            specie_id = None
+                        # Add characters if not in DB
+                        db.session.add(Characters(
+                            id=int(character_id),
+                            name=charac["name"],
+                            birth_year=charac["birth_year"],
+                            gender=charac["gender"],
+                            planet_id=planet_id,
+                            specie_id=specie_id
+                        ))
+                    db.session.commit()
+                    characters = db.session.scalars(
+                        db.select(Characters)).all()
+                    # Serialize characters objet.
+                    response_body = [c.serialize() for c in characters]
+                    return jsonify(response_body), 201
+                else:
+                    response_body[
+                        'error'] = f'Error retrieving SWAPI data. Code status: {response.status_code}'
+                    return response_body
+            else:
+                # Get charac if its in DB + serialize characters objet
+                response_body = [c.serialize() for c in characters]
+                return jsonify(response_body), 200
 
         elif request.method == "POST":
             data = request.json
@@ -33,57 +66,19 @@ def register_characters_routes(app):
             character = db.session.get(Characters, character_id)
 
             if not character:
-                url = f'https://swapi.info/api/people/{character_id}'
-                response = requests.get(url)
-
-                if response.status_code == 200:
-                    data = response.json()
-                    # Check if charact exisits in DB
-                    existing_char = db.session.scalar(
-                        db.select(Characters).filter_by(id=character_id))
-
-                    if not existing_char:
-                        # get ID from url of homeworld property's value
-                        planet_id = int(data["homeworld"].split("/")[-1])
-
-                        if len(data["species"]) != 0:
-                            specie_id = int(data["species"][0].split("/")[-1])
-                        else:
-                            specie_id = None
-
-                        # Create new charac if its not in DB
-                        char = Characters(
-                            id=int(character_id),
-                            name=data["name"],
-                            birth_year=data["birth_year"],
-                            gender=data["gender"],
-                            planet_id=planet_id,
-                            specie_id=specie_id
-                        )
-                        db.session.add(char)
-                        db.session.commit()
-                    else:
-                        # Get charac if its in DB
-                        char = existing_char
-
-                    # Serialize charac objet.
-                    response_body['results'] = char.serialize()
-                else:
-                    response_body[
-                        'error'] = f'Error retrieving SWAPI data. Code status: {response.status_code}'
-
-                return response_body, 200
-
+                response_body[
+                    'error'] = f'Character ID:{character_id} doesnt exist.'
             else:
-                result = character.serialize()
-                return jsonify(result)
+                response_body["result"] = character.serialize()
+
+            return jsonify(response_body), 200
 
         elif request.method == "DELETE":
             delete_char = db.session.scalar(
                 db.select(Characters).filter_by(id=character_id))
 
             if not delete_char:
-                response_body['message'] = f'The character with ID:{character_id} wasnt found.'
+                response_body['message'] = f'Character ID:{character_id} wasnt found.'
                 return response_body, 404
             else:
                 db.session.delete(delete_char)
--- src/routes/species.py
+++ src/routes/species.py
@@ -2,6 +2,7 @@ from flask import request, jsonify
 from models import db, Species
 import requests
 
+
 def register_species_routes(app):
 
     @app.route("/species", methods=["GET", "POST"])
@@ -9,6 +10,7 @@ def register_species_routes(app):
         response_body = {}
         if request.method == "GET":
             species = db.session.scalars(db.select(Species)).all()
+
             if not species:
                 url = 'https://swapi.info/api/species'
                 response = requests.get(url)
@@ -19,10 +21,10 @@ def register_species_routes(app):
                     for specie in data:
                         specie_id = int(specie["url"].split("/")[-1])
                         db.session.add(Species(
-                            id= specie_id,
-                            name= specie["name"],
-                            designation= specie["designation"],
-                            language= specie["language"]
+                            id=specie_id,
+                            name=specie["name"],
+                            designation=specie["designation"],
+                            language=specie["language"]
                         ))
                     db.session.commit()
                     species = db.session.scalars(db.select(Species)).all()
@@ -35,7 +37,7 @@ def register_species_routes(app):
             else:
                 response_body = [s.serialize() for s in species]
                 return jsonify(response_body), 200
-                     
+
         elif request.method == "POST":
             data = request.json
             species = Species(**data)
@@ -46,12 +48,17 @@ def register_species_routes(app):
 
     @app.route("/species/<int:specie_id>", methods=["GET", "DELETE"])
     def specie_item(specie_id):
-        specie = db.session.get(Species, specie_id)
-        if not specie:
-            return {"error": "Specie not found"}, 404
+        response_body = {}
         if request.method == "GET":
-            result = specie.serialize()
-            return jsonify(result)
+            specie = db.session.get(Species, specie_id)
+            if not specie:
+                response_body[
+                    'error'] = f'Specie ID:{specie_id} doesnt exist.'
+            else:
+                response_body["result"] = specie.serialize()
+
+            return jsonify(response_body), 200
+
         elif request.method == "DELETE":
             db.session.delete(specie)
             db.session.commit()
